46cc5285fd36651a819577ee1065a7a53e56dab7
unknown <vga83285@B24-WS003.dc.diamond.ac.uk>
2014-11-21 14:23:55 +0000

    Added camera definitions - cameras.py.

---
+++ cameras.py
@@ -0,0 +1,6 @@
+## _cameras [(label, dsp line, ip address, model)]
+cameras = [
+    ('West', 8906, 1<<0, 'ixon1.b24', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585]),
+    ('East', 8974, 1<<1, 'ixon2.b24', 7777, 'ixon', ['Cy5', 'FITC'], [670, 518]),
+    ]
+camera_keys = ['label', 'serial', 'line', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
